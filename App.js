26f5ab141b0c898f52cb41c2b117532af2748302
Sridevi Patchipulusu <sridevips@hotmail.com>
2020-01-30 21:28:50 -0600

    Components Part 2

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
- import React from 'react';
+import React from 'react';
 import Main from './components/MainComponent';
 
 export default function App() {
@@ -8,24 +8,3 @@ export default function App() {
     );
 }   
 
-/* import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-  
-      <Text>hello</Text>
-    </View>
-  );
-}
-
- const styles = StyleSheet.create({
- container: {
-   flex: 1,
-     backgroundColor: '#fff',
-   alignItems: 'center',
-     justifyContent: 'center',
-  },
- });  
-   */
