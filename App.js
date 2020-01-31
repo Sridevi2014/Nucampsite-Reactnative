8b1327683c291d1a2acd43d42b6cf722dfb9f12c
Sridevi Patchipulusu <sridevips@hotmail.com>
2020-01-30 21:01:55 -0600

    Components Part 1

---
+++ App.js
@@ -0,0 +1,31 @@
+
+ import React from 'react';
+import Main from './components/MainComponent';
+
+export default function App() {
+    return (
+        <Main />
+    );
+}   
+
+/* import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+  
+      <Text>hello</Text>
+    </View>
+  );
+}
+
+ const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+     backgroundColor: '#fff',
+   alignItems: 'center',
+     justifyContent: 'center',
+  },
+ });  
+   */
